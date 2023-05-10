783437cdb7bfa20360370bc921a78a5a07ebfce3
Guillarda <65513597+Guillarda@users.noreply.github.com>
2023-05-10 16:21:12 +0200

    Update YTDLPMDL.bat

--- YTDLPMDL.bat
+++ YTDLPMDL.bat
@@ -3,7 +3,8 @@ title YT-DLP Music Downloader By Guillarda
 yt-dlp -U
 :home
 Set /p link=What is the link of your wanted Video or Playlist (please one by one only) : 
-echo This is your link : "%link%"
+echo This is the link you entered : "%link%"
+echo.
 yt-dlp -x --audio-format mp3 --embed-metadata --embed-thumbnail -o "Downloads/%%(title)s" "%link%"
-ECHO.
+echo.
 Goto home
