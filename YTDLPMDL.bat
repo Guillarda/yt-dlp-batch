256ba60d9a5e2495302cd1f186e61c6c0f3d7c45
Guillarda <65513597+Guillarda@users.noreply.github.com>
2023-05-17 13:22:58 +0200

    Update of the script
    Now the script download yt-dlp if you don't have it
    And ffmpeg, wget is in bin folder

--- YTDLPMDL.bat
+++ YTDLPMDL.bat
@@ -1,6 +1,14 @@
 @echo off
 title YT-DLP Music Downloader By Guillarda
-yt-dlp -U
+set "Path=%Path%;%CD%\bin;"
+yt-dlp --version > nul
+if %errorlevel% NEQ 0 (
+    cls
+    echo Downloading of yt-dlp
+    "./bin/wget.exe" https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe -O "./bin/yt-dlp.exe" -q
+) else (
+    goto :home
+)
 :home
 Set /p link=What is the link of your wanted Video or Playlist (please one by one only) : 
 echo This is the link you entered : "%link%"
